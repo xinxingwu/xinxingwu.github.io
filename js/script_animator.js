ceea16e3cba7709d60ac801f8f52d8847e6d0e38
Xinxing Wu <xinxingwu@yeah.net>
2025-03-24 17:26:02 -0400

    Add files via upload

---
+++ js/script_animator.js
@@ -0,0 +1,13 @@
+const pupils = document.querySelectorAll('.pupil');
+
+document.addEventListener('mousemove', (e) => {
+  const centerX = window.innerWidth / 2;
+  const centerY = window.innerHeight / 2;
+  const dx = (e.clientX - centerX) / 100;
+  const dy = (e.clientY - centerY) / 100;
+
+  pupils.forEach(pupil => {
+    pupil.style.setProperty('--dx', `${dx}px`);
+    pupil.style.setProperty('--dy', `${dy}px`);
+  });
+});
